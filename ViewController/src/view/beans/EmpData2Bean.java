20f447f83749911ea631bca885d67080de6bec90
Mahmoud <Mahmoud@DESKTOP-GNCLC3A>
2018-02-27 06:49:32 +0300

    Com1

--- ViewController/src/view/beans/EmpData2Bean.java
+++ ViewController/src/view/beans/EmpData2Bean.java
@@ -25,7 +25,7 @@ public class EmpData2Bean {
     private RichOutputText outBindToSetVal;
 
     public EmpData2Bean() {
-    }//99999999999999999
+    }//9999999999999999954654654645
 
     public void setPassword(String password) {
         setMsg("Setting Password");
